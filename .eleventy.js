8cdbeefa18933afed45aaf0b97b3838b0669e5ad
Henry Desroches <henrydesroches@google.com>
2022-06-02 17:31:46 -0400

    🎨 - Basic style setup

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("fonts");
-    eleventyConfig.addFilter('website', str => {
-        return str.replace('a website', `<span>a website</span>`)
-    })
-    return {
-        htmlTemplateEngine: "njk"
-    }
-}
+  eleventyConfig.addPassthroughCopy('fonts');
+  eleventyConfig.addPassthroughCopy('img');
+  eleventyConfig.addPassthroughCopy('*.css');
+  eleventyConfig.addFilter('website', str => {
+    return str.replace('a website', `<span>a website</span>`);
+  });
+  return {
+    htmlTemplateEngine: 'njk',
+  };
+};
