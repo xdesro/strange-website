1d5a54579847d13bbfd24b25ca95afa58275eada
Henry Desroches <henrydesroches@google.com>
2022-06-03 13:22:43 -0400

    🔎 - Add meta and favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('meta');
   eleventyConfig.addPassthroughCopy('fonts');
   eleventyConfig.addPassthroughCopy('img');
   eleventyConfig.addPassthroughCopy('*.css');
